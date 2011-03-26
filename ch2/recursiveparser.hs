79e4f8a03cd67d2e1b0bfffaf36630838abc6601
Tianyi Cui <tianyicui@gmail.com>
2011-03-26 10:53:31 +0800

    recursiveparser.hs

---
+++ ch2/recursiveparser.hs
@@ -0,0 +1,71 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment (getArgs)
+import Control.Monad (liftM)
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+               deriving (Show, Eq)
+
+parseString :: Parser LispVal
+parseString = do
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = first : rest
+    return $ case atom of
+               "#t" -> Bool True
+               "#f" -> Bool False
+               otherwise -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+        <|> parseQuoted
+        <|> do char '('
+               x <- try parseList <|> parseDottedList
+               char ')'
+               return x
+
+symbol :: Parser Char
+symbol = oneOf "!$%$}*+-/:<=?>@^_~#"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value: " ++ show val
+
+main :: IO ()
+main = do args <- getArgs
+          putStrLn . readExpr . head $ args
